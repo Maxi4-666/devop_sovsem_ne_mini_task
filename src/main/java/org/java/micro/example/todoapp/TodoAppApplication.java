71bd9c1ba93063a726d7e74d40a0b73d85fe75ee
Max <maksimli722@gmail.com>
2025-12-03 23:56:19 +0500

    first commit

---
+++ src/main/java/org/java/micro/example/todoapp/TodoAppApplication.java
@@ -0,0 +1,13 @@
+package org.java.micro.example.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppApplication.class, args);
+    }
+
+}
